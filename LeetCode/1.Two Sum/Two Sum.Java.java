4c6295310354d58826912c79d44cce32307b319f
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-01-05 15:48:58 +0900

    solve: LeetCode 1 (Easy)

---
+++ LeetCode/1.Two Sum/Two Sum.Java.java
@@ -0,0 +1,19 @@
+// Problem: 1. Two Sum (Easy)
+// Link: https://leetcode.com/problems/two-sum
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int y = target - nums[i];
+
+            if (map.containsKey(y)) {
+                return new int[]{map.get(y), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{0, 0};
+    }
+}
