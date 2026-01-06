4e0ed94972dd8288d40217bf7f5f7d376560f505
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-01-06 20:27:50 +0900

    [level 2] Title: 피로도, Time: 5.69 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -7,23 +7,20 @@ class Solution {
         answer = Integer.MIN_VALUE;
         dgs = dungeons;
         visited = new boolean[dgs.length];
-        backtracking(0, k, 0);
+        backtracking(k, 0);
         return answer;
     }
     
-    private void backtracking(int count, int currentP, int num) {
-        if (count == dgs.length) {
-            answer = Math.max(answer, num);
-            return;
-        }
+    private void backtracking(int currentP, int num) {
+        if (num > dgs.length) return;
+        answer = Math.max(answer, num);
+        
         for (int i = 0; i < dgs.length; i++) {
             if (visited[i]) continue;
-            int cp = dgs[i][0];
-            int sp = dgs[i][1];
             visited[i] = true;
-            boolean checkP = false;
-            if (currentP >= cp) checkP = true; 
-            backtracking(count + 1, checkP ? currentP - sp : currentP, checkP ? num + 1 : num);
+            if (currentP >= dgs[i][0]) {
+                backtracking(currentP - dgs[i][1], num + 1);
+            }
             visited[i] = false;
         }
     }
