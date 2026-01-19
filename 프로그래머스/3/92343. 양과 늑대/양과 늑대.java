bbe34326b242ad71a25f52ea912ab8cdd0c2f251
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-01-19 13:05:28 +0900

    [level 3] Title: 양과 늑대, Time: 0.74 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -8,36 +8,30 @@ class Solution {
         for (int i = 0; i < edges.length; i++) {
             list.get(edges[i][0]).add(edges[i][1]);
         }
-        
+    
         Queue<int[]> queue = new ArrayDeque<>();
-        // 장바구니, 양의 개수, 늑대의 개수
-        queue.add(new int[]{1 << 0, 1, 0});
-        boolean[] visitedState = new boolean[1 << len];
-        visitedState[1 << 0] = true;
-
+        queue.add(new int[]{(1 << 0), 1, 0});
+        boolean[] visited = new boolean[1 << len];
+        visited[1 << 0] = true;
+        
         while(!queue.isEmpty()) {
-            int[] arr = queue.poll();
+            int[]arr = queue.poll();
             int mask = arr[0];
             int sheep = arr[1];
             int wolf = arr[2];
+            
             answer = Math.max(answer, sheep);
             
             for (int i = 0; i < len; i++) {
-                // 만약 i번째 노드가 현재 방문한 상태(mask)라면?
-                if ((mask & (1 << i)) != 0) {
-                    for (int num:list.get(i)) {
-
-                        int next = mask | (1 << num);
-                        // 방문 한 곳인지 확인해야 한다..
-                        if (!visitedState[next]) {
-                            // 큐에 넣는다.
-int nextSheep = sheep + (info[num] == 0 ? 1 : 0);
+                if (((1 << i) & mask) != 0) {
+                    for (int num : list.get(i)) {
+                        int nextMask = (1 << num) | mask;
+                        if (!visited[nextMask]) {
+                            int nextSheep = sheep + (info[num] == 0 ? 1 : 0);
                             int nextWolf = wolf + (info[num] == 1 ? 1 : 0);
-                            
-                            // 늑대에게 잡히지 않는다면 큐에 추가!
                             if (nextSheep > nextWolf) {
-                                visitedState[next] = true;
-                                queue.add(new int[]{next, nextSheep, nextWolf});
+                                visited[nextMask] = true;
+                                queue.add(new int[]{nextMask, nextSheep, nextWolf});
                             }
                         }
                     }
