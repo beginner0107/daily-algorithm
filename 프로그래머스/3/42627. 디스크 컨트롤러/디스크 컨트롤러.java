e6d363176f73035b1a634dd340afcbf6168a6b48
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-02-02 15:31:18 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.86 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,33 +1,46 @@
 import java.util.*;
+
 class Solution {
+    public class Job implements Comparable<Job>{
+        int idx; // 작업 번호
+        int rt; // requestTime 요청 시간
+        int dt; // duration 소요 시간
+        
+        Job(int idx, int rt, int dt) {
+            this.idx = idx;
+            this.rt = rt;
+            this.dt = dt;
+        }
+        
+        @Override
+        public int compareTo(Job o) {
+            if (this.dt != o.dt) return Integer.compare(this.dt, o.dt);
+            if (this.rt != o.rt) return Integer.compare(this.rt, o.rt);
+            return Integer.compare(this.idx, o.idx);
+        }
+    }
+    
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+        Queue<Job> pq = new PriorityQueue<>();
         
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
-            if (a[1] != b[1]) return Integer.compare(a[1], b[1]);
-            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
-            return Integer.compare(a[2], b[2]);
-        });
-        
-        int completed = 0;
         int currentTime = 0;
         int totalTime = 0;
-        int jobIndex = 0;
-        
-        while(completed < jobs.length) {
-            while (jobIndex < jobs.length && currentTime >= jobs[jobIndex][0]) {
-                queue.add(new int[]{jobs[jobIndex][0], jobs[jobIndex][1], jobIndex});
-                jobIndex++;
+        int index = 0;
+        int count = 0;
+        while (count < jobs.length) {
+            while (index < jobs.length && currentTime >= jobs[index][0]) {
+                pq.add(new Job(index, jobs[index][0], jobs[index][1]));
+                index++;
             }
-            
-            if (queue.isEmpty()) {
-                currentTime = jobs[jobIndex][0];
+            if (pq.isEmpty()) {
+                currentTime = jobs[index][0];
             }
             else {
-                int[] arr = queue.poll();
-                currentTime += arr[1];
-                totalTime += (currentTime - arr[0]);
-                completed++;
+                Job cur = pq.poll();
+                currentTime += cur.dt; // 소요 시간을 더해준다.
+                totalTime += (currentTime - cur.rt);
+                count++;
             }
         }
         return totalTime / jobs.length;
