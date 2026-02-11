75f2868c02455fe9f2658f1fb7f23d7aa34fc496
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-02-11 21:58:16 +0900

    solve: LeetCode 62 (Medium)

---
+++ LeetCode/62.Unique Paths/Unique Paths.java
@@ -0,0 +1,25 @@
+// Problem: 62. Unique Paths (Medium)
+// Link: https://leetcode.com/problems/unique-paths
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    grid[i][j] = 1;
+                }
+                if (j == 0) {
+                    grid[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
