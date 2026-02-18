246a0217cfb0f6911adf4c31770e73d8d2a5cf62
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-02-18 21:09:19 +0900

    solve: LeetCode 1143 (Medium)

--- LeetCode/1143.Longest Common Subsequence/Longest Common Subsequence.java
+++ LeetCode/1143.Longest Common Subsequence/Longest Common Subsequence.java
@@ -7,17 +7,27 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int len1 = text1.length();
         int len2 = text2.length();
-        int[][]dp = new int[len1 + 1][len2 + 1];
+        if (len1 > len2) {
+            String tmp = text1;
+            text1 = text2;
+            text2 = tmp;
+        }
+        len1 = text1.length();
+        len2 = text2.length();
+        int[]dp = new int[len1 + 1];
         
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+        for (int i = 1; i <= len2; i++) {
+            int prev = 0;
+            for (int j = 1; j <= len1; j++) {
+                int tmp = dp[j];
+                if (text1.charAt(j - 1) == text2.charAt(i - 1)) {
+                    dp[j] = prev + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
                 }
+                prev = tmp;
             }
         }
-        return dp[len1][len2];    
+        return dp[len1];    
     }
 }
