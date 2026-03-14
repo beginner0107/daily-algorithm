2a031d1dad5a7465f537904b0b9f3cd34d2558ea
Beginner <81161819+beginner0107@users.noreply.github.com>
2026-03-14 15:25:44 +0900

    solve: LeetCode 1 (Easy)

--- LeetCode/1.Two Sum/Two Sum.java
+++ LeetCode/1.Two Sum/Two Sum.java
@@ -4,33 +4,18 @@
 import java.util.*;
 
 class Solution {
-   private Stack<Integer> st = new Stack<>();
-    private int tg;
-    private int len;
-
     public int[] twoSum(int[] nums, int target) {
-        st.clear();
-        len = nums.length;
-        tg = target;
+        Map<Integer, Integer> map = new HashMap<>();
 
-        if (backTracking(nums, 0)) {
-            return new int[]{st.get(0), st.get(1)};
-        }
-        return new int[]{};
-    }
-
-    private boolean backTracking(int[] nums, int sIndex) {
-        if (st.size() == 2) {
-            return (nums[st.get(0)] + nums[st.get(1)]) == tg;
-        }
-
-        for (int i = sIndex; i < len; i++) {
-            st.add(i);
-            if (backTracking(nums, i + 1)) {
-                return true;
+        int idx = 0;
+        for (int num : nums) {
+            int tmp = target - num;
+            if (map.containsKey(tmp)) {
+                return new int[]{map.get(tmp), idx};
             }
-            st.pop();
+            map.put(num, idx);
+            idx++;
         }
-        return false;
+        return null;
     }
 }
